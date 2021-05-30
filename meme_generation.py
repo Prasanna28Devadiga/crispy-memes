4b519c5809d76993ac7781ffdfbb60748e9c0bd4
charles-vj <71175155+charles-vj@users.noreply.github.com>
2021-05-30 11:03:22 +0530

    Add files via upload

---
+++ meme_generation.py
@@ -0,0 +1,19 @@
+import requests
+
+url = "https://api.imgflip.com/caption_image"
+i=598
+lc=0
+text0 =""
+text1=""
+with open("kc.txt", "r") as a_file:
+  for line in a_file:
+      text0 = line.strip()
+      querystring = {"template_id":"1202623","username":"PrasannaDevadiga","password":"jennytalia","text0":text0,"text1":""}
+      response = requests.post(url,data=querystring )
+      url_i=response.json()['data']['url']
+      response = requests.get(url_i)
+      file = open(str(i)+".png", "wb")
+      file.write(response.content)
+      file.close()
+      i=i+1
+      lc=0

c1c267ebdcb8012b17f4e26bd5a825be1db6a0a5
Prasanna28Devadiga <54196612+Prasanna28Devadiga@users.noreply.github.com>
2021-05-29 22:36:36 -0700

    Add files via upload

---
+++ text_generation.py
@@ -0,0 +1,17 @@
+import requests
+
+url = "https://api.imgflip.com/caption_image"
+
+querystring = {"template_id":"112126428","username":"PrasannaDevadiga","password":"jennytalia","text0":"Testing","text1":"Testes"}
+
+
+
+response = requests.post(url,data=querystring )
+
+url_i=response.json()['data']['url']
+
+response = requests.get(url_i)
+
+file = open("sample_image.png", "wb")
+file.write(response.content)
+file.close()
